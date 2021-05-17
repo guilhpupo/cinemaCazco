0df747ce93db22bf7f35bf07878ad058c5a56d9d
Guilherme Pupo <guilh.pupo@gmail.com>
2021-05-17 04:50:52 -0300

    changed requests from http to https

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -3,7 +3,7 @@ import Axios from 'axios'
 const REACT_APP_OMDB_APIKEY = process.env.REACT_APP_OMDB_APIKEY
 
 export const Api = Axios.create({
-  baseURL: `http://www.omdbapi.com/`,
+  baseURL: `https://www.omdbapi.com/`,
   params: {
     apikey: REACT_APP_OMDB_APIKEY
   }
